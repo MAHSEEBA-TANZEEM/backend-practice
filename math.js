0eac04bf1b6121ae09009d22378fd9e5dcad2361
MAHSEEBA TANZEEM <mahseebatanzeem777@gmail.com>
2024-10-29 21:56:54 +0530

    Initial Commit

---
+++ math.js
@@ -0,0 +1,48 @@
+// exports in files
+
+// const sum=(a,b) => a+b;
+// const mul=(a,b) => a*b;
+// const g = 9.8;
+// const PI = 3.14;
+// module.exports = 123;
+
+// module.exports = "hello";
+
+// let obj = {
+//     sum : sum,
+//     mul : mul,
+//     g : g,
+//     PI : PI
+// };
+
+// module.exports = obj;
+
+//  module.exports = {
+//     sum : sum,
+//     mul : mul,
+//     g : g,
+//     PI : PI
+// };
+
+// module.exports.sum = (a,b) => a+b;
+// module.exports.mul = (a,b) => a*b;
+// module.exports.PI = 3.14;
+// module.exports.g = 9.8;
+
+// exports.sum = (a,b) => a+b;
+// exports.mul = (a,b) => a*b;
+// exports.PI = 3.14;
+// exports.g = 9.8;
+
+// exports = 5;
+
+// exports in directories
+
+// import modules
+
+export const sum=(a,b) => a+b;
+export const mul=(a,b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
+
+
---
+++ script.js
@@ -0,0 +1,41 @@
+// let n=5;
+
+// for(let i=0; i<n; i++) {
+//     console.log("hello,",i);
+// }
+
+// console.log("bye!");
+
+// console.log(process.argv);
+
+// let args= process.argv;
+
+// for(let i=2; i<args.length; i++) {
+//     console.log("hello to",args[i]);
+// }
+
+// const someValue = require("./math");
+// console.log(someValue);
+
+// const math = require("./math");
+// console.log(math);
+
+// console.log(math.sum(2,2));
+// console.log(math.PI);
+
+// console.log(math.g);
+
+// export in directories
+// const info = require("./Fruits");
+
+// console.log(info);
+// console.log(info[0]);
+// console.log(info[0].name);
+
+// import in modules
+
+// import {sum,PI} from "./math.js";
+// console.log(sum(1,2));
+
+import{generate} from "random-words";
+console.log(generate());
